ea64a437241257862a785004ea3d8e2a879e4417
abhinand399 <mrcapzi58@gmail.com>
2026-01-05 22:12:14 +0530

    Day 2: added largest_of_3num program

---
+++ largest_of_3num.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter First number"))
+num2=int(input("Enter 2nd number"))
+num3=int(input("Enter 3rd number"))
+
+if num1>num2>num3:
+    print("num1 is largest")
+
+elif num2>num3:
+    print("num2 is largest")
+    
+else:
+    print("num3 is largest")
+
+
